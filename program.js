3ff193dcc78c24a0d7dae276a4165a4635cfb676
David Gomez <dgomez@hugeinc.com>
2015-10-29 15:17:39 -0500

    session

--- program.js
+++ program.js
@@ -7,10 +7,12 @@ const app = koa();
 
 // to use signed cookie, we need to set app.keys
 app.keys = ['secret', 'keys'];
+app.use(session(app));
+
 app.use(function *(){
   const view = ~~this.cookies.get('view', { signed: true }) + 1;
   this.cookies.set('view', view, { signed: true });
-  this.body = view + ' views';
+  this.body = `${view} views`;
 });
 // ~~ it is used as a faster substitute for Math.floor().
 
