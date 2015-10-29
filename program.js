d6c3675a174d5d3b4b8bdac8cd80d037929f468d
David Gomez <dgomez@hugeinc.com>
2015-10-29 11:56:58 -0500

    content headers

--- program.js
+++ program.js
@@ -5,28 +5,9 @@ const parse = require('co-body');
 const app = koa();
 
 app.use(function *(next) {
-  // only accept POST request
-  if (this.method !== 'POST') return yield next;
+  if (this.path !== '/') return yield next;
 
-  // max body size limit to `1kb`
-  const body = yield parse(this, { limit: '1kb' });
-
-  // if body.name not exist, respond `400`
-  if (!body.name) this.throw(400, '.name required');
-
-  this.body = body.name.toUpperCase();
-});
-
-app.use(function *(next) {
-  if (this.path !== '/json') return yield next;
-
-  this.body = { foo: 'bar' };
-});
-
-app.use(function *(next) {
-  if (this.path !== '/stream') return yield next;
-
-  this.body = fs.createReadStream(process.argv[3]);
+  this.body = this.request.is('application/json') ? {message: 'hi!'} : 'ok';
 });
 
 app.listen(process.argv[2]);
