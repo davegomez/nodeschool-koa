ee571c165c0396a292b1040a6d9c4caa11cdae00
David Gomez <dgomez@hugeinc.com>
2015-10-29 10:35:41 -0500

    routing refactor

--- program.js
+++ program.js
@@ -1,28 +1,26 @@
 const koa = require('koa');
 const app = koa();
+const parse = require('co-body');
 
 app.use(function* (next) {
-  if (this.path !== '/') {
-    return yield next;
-  }
-
-  this.body = 'hello koa';
+  if (this.path !== '/') return yield next;
+  const body = yield parse(this);
+  this.body = body.name.toUpperCase();
 });
 
 app.use(function* (next) {
-  if (this.path !== '/404') {
-    return yield next;
-  }
-
+  if (this.path !== '/404') return yield next;
   this.body = 'page not found';
 });
 
 app.use(function* (next) {
-  if (this.path !== '/500') {
-    return yield next;
-  }
-
+  if (this.path !== '/500') return yield next;
   this.body = 'internal server error';
+});
+
+app.use(function* (next) {
+  if (this.path !== '/test') return yield next;
+  this.body = JSON.stringify({ name: 'Dave' });
 });
 
 app.listen(process.argv[2]);
